bd5fa11bd8a4f2eac2302f70f12e02ae878afa74
filippob <filippo.biscarini@gmail.com>
2020-02-26 19:12:57 +0100

    added scripts for example snakemake pipelines

---
+++ 7.pipeline/example_pipeline/command_lines.sh
@@ -0,0 +1,34 @@
+## visualize the help manual for snakemake
+snakemake --help
+
+## input data (unsorted snp file)
+less snp.map
+
+## dry runs for the first example_pipeline (long and short options) 
+snakemake --dryrun --snakefile example_snakefile
+snakemake -n -s example_snakefile
+
+## dry run to produce a DAG of the pipeline
+snakemake --dag --dryrun --snakefile example_snakefile | dot -Tsvg > dag_example.svg
+
+## actual run of the first example pipeline
+snakemake --snakefile example_snakefile
+
+## output data
+zless snp_sorted.map.gz
+
+## unzip sorted snp file
+gunzip snp_sorted.map.gz
+
+## rerun the first example pipeline: dry run for DAG
+snakemake --dag --dryrun --snakefile example_snakefile | dot -Tsvg > dag_example_unzipped.svg
+
+## rerun the first example pipeline: actual run for DAG
+snakemake --snakefile example_snakefile
+
+## run the second example pipeline: dry run for DAG
+snakemake --dag -n --snakefile example_snakefile_2 | dot -Tsvg > dag_example_2.svg
+
+## run the second example pipeline: actual run
+snakemake --snakefile example_snakefile_2
+
