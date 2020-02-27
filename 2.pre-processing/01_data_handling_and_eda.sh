cb661b7976ce78f14d880cf6ef24a2abea4876fd
christian werner <christian.werner@roslin.ed.ac.uk>
2020-02-27 16:10:52 +0000

    added comments

--- 2.pre-processing/01_data_handling_and_eda.sh
+++ 2.pre-processing/01_data_handling_and_eda.sh
@@ -39,19 +39,19 @@ $plink --dog --file ../data/dogs --freqx --out dogs
 
 ## stratified allele frequencies
 
-$plink --file ../data/rice --update-ids update.ids --recode --out rice_pop
-$plink --file rice_pop --freq --family --out rice_pop ## frequency by population
-$plink --dog --file ../data/dogs --freq case-control --allow-no-sex --out dogs
+$plink --file ../data/rice --update-ids update.ids --recode --out rice_pop  # update.ids file updates the id's in the pop and map files
+$plink --file rice_pop --freq --family --out rice_pop # MAF and frequency by population (fam IDs)
+$plink --dog --file ../data/dogs --freq case-control --allow-no-sex --out dogs #case/control phenotype-stratified report
 
 
 ## missing rate
 
-$plink --file ../data/rice --missing --out rice
+$plink --file ../data/rice --missing --out rice # missing markers per ind and missing SNP calls
 $plink --dog --file ../data/dogs --missing --out dogs
 
 ## Hardy-Weinberg equilibrium
 
-$plink --file ../data/rice --hardy --out rice
+$plink --file ../data/rice --hardy --out rice # observed and expected heterozygosities under HWE
 $plink --dog --file ../data/dogs --hardy --out dogs
 
 $plink --file ../data/rice --het --out rice
