db8cf383c3bc29661e77b22d813e95e8ed4dad70
filippob <filippo.biscarini@gmail.com>
2020-03-03 18:10:15 +0100

    pipeline for the dogs dataset added to repo

---
+++ 7.pipeline/snakefile_command_line.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## prepare the env
+
+mkdir data 2>/dev/null
+mkdir steps 2>/dev/null
+cp -r ../software .
+cp ../4.gwas/gwas_rrblup.R software
+cp ../1.preparatory_steps/prep_dogpheno.R software
+
+## dry run
+#snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs.svg
+## full run
+snakemake -s Snakefile_GWAS.binary
