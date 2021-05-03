a23177ab453e3dbcd8bfb5013b60bcfd7617b6c8
filippob <filippo.biscarini@gmail.com>
2021-05-03 12:27:12 +0200

    modified gwas pipeline for parus data

--- 7.pipeline/snakefile_command_line_assignment.sh
+++ 7.pipeline/snakefile_command_line_assignment.sh
@@ -4,9 +4,9 @@
 mkdir data 2>/dev/null
 mkdir steps 2>/dev/null
 cp -r ../software .
-cp ../4.gwas/gwas_rrblup.R software
+cp ../4.gwas/gwas_* software ## which script do we want to use for GWAS?
 
 ## dry run
-#snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs.svg
+snakemake --dag -n -s Snakefile_GWAS.parus | dot -Tsvg > dag_parus.svg
 ## full run
 snakemake -s Snakefile_GWAS.parus
