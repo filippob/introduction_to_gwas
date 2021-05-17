da97ab2b3c5f62dce7f7c51df343f6a07908e767
WERNER <c.werner@cgiar.org>
2021-05-17 10:12:23 +0100

    updated R installation file and Shell intro slides

--- 1.preparatory_steps/packages_to_install.R
+++ 1.preparatory_steps/packages_to_install.R
@@ -1,10 +1,26 @@
 ## list of R packages to install for the course
 ## (some additional packages may still be needed during the course)
 
+
+## from Bioconductor
+source("http://bioconductor.org/biocLite.R")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+
+BiocManager::install("biomaRt")
+BiocManager::install("snpStats")
+BiocManager::install("qvalue")
+
+
+## doMC
+install.packages("doMC", repos="http://R-Forge.R-project.org")
+
+
 ## from CRAN
 install.packages("AER")
 install.packages("msm")
-install.packages("doMC")
 install.packages("nnet")
 install.packages("MASS")
 install.packages("afex")
@@ -16,13 +32,11 @@ install.packages("plotly")
 install.packages("Matrix")
 install.packages("coxmeg")
 install.packages("rrBLUP")
-install.packages("qvalue")
 install.packages("lmtest")
 install.packages("ggplot2")
 install.packages("R.utils")
 install.packages("genetics")
 install.packages("survival")
-install.packages("snpStats")
 install.packages("reshape2")
 install.packages("prettydoc")
 install.packages("patchwork")
@@ -37,19 +51,11 @@ install.packages("statgenGWAS")
 install.packages("RColorBrewer")
 
 
-
 ## from the github repository (to be installed after cloning the repo)
 install.packages("../software/gMatrix_0.2.tar.gz", repos = NULL, type = "source")
 install.packages("../software/ldDesign_2.0-1.tar.gz", repos = NULL, type = "source")
-install.packages("../software/GenABEL.data_1.0.0.tar.gz", repos = NULL, type = "source")
-install.packages("../software/GenABEL_1.8-0.tar.gz", repos = NULL, type = "source")
+#install.packages("../software/GenABEL.data_1.0.0.tar.gz", repos = NULL, type = "source")
+#install.packages("../software/GenABEL_1.8-0.tar.gz", repos = NULL, type = "source")
 
-## from Bioconductor
-source("http://bioconductor.org/biocLite.R")
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install()
-
-BiocManager::install("biomaRt")
 
