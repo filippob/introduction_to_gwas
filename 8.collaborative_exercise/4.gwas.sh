e18e36255d492f132fc9f026efaa3b0ee555fa46
filippob <filippo.biscarini@gmail.com>
2021-05-31 10:09:33 +0200

    clean scripts for the collaborative exercise

--- 8.collaborative_exercise/4.gwas.sh
+++ 8.collaborative_exercise/4.gwas.sh
@@ -2,14 +2,6 @@
 ## GWAS ##
 #----------
 
-plink=/home/filippo/Downloads/plink
 
-## prepare data for gwas.R
-$plink --file pigs_imputed --recode A --out pigs_imputed
-
-
-## GWAS
-Rscript --vanilla ../4.gwas/gwas_statgengwas.R genotype_file=pigs_imputed.raw snp_map=pigs_imputed.map phenotype_file=data/pigs_phenotypes.txt trait=phenotype trait_label=stmp_tail
-Rscript --vanilla ../4.gwas/gwas_rrblup.R genotype_file=pigs_imputed.raw snp_map=pigs_imputed.map phenotype_file=data/pigs_phenotypes.txt trait=phenotype trait_label=stump_tail
 
 
