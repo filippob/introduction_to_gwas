5a3960048d4e6f1c3a2b46c14f6b92b1c2654c11
filippob <filippo.biscarini@gmail.com>
2021-06-22 17:57:01 +0200

    updated 4.gwas.sh

--- 6.steps/4.gwas.sh
+++ 6.steps/4.gwas.sh
@@ -4,10 +4,11 @@
 
 plink=plink
 
-$plink --file ../3.imputation/rice_imputed --recode A --out rice_imputed
-$plink --dog --file ../3.imputation/dogs_imputed --recode A --out dogs_imputed
+$plink --file rice_imputed --recode A --out rice_imputed
+$plink --dog --file dogs_imputed --recode A --out dogs_imputed
 
 ## stand-alone script
-Rscript --vanilla gwas_rrblup.R genotype_file=dogs_imputed.raw snp_map=../3.imputation/dogs_imputed.map phenotype_file=../data/dogs_phenotypes.txt trait=phenotype trait_label=cleft_lip
-Rscript --vanilla gwas_rrblup.R genotype_file=rice_imputed.raw snp_map=../3.imputation/rice_imputed.map phenotype_file=../data/rice_phenotypes.txt trait=PH trait_label=PH
-
+Rscript --vanilla gwas_rrblup.R genotype_file=dogs_imputed.raw snp_map=dogs_imputed.map phenotype_file=data/dogs_phenotypes.txt trait=phenotype trait_label=cleft_lip
+Rscript --vanilla gwas_sommer.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=data/rice_phenotypes.txt trait=PH trait_label=plant_height covariates=population
+Rscript --vanilla gwas_statgengwas.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=data/rice_phenotypes.txt trait=PH trait_label=plant_height systematic_effects=population
+Rscript --vanilla gwas_rrblup.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=data/rice_phenotypes.txt trait=PH trait_label=plant_height
