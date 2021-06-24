bab6bd85c59549ec840525fd3dfe94afcd7a2713
filippob <filippo.biscarini@gmail.com>
2021-06-24 18:25:30 +0200

    bash script to prepare the env for binary pipeline

--- 7.pipeline/snakefile_command_line_binary.sh
+++ 7.pipeline/snakefile_command_line_binary.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ## prepare the env
 
+mkdir data
+mkdir steps
+cp -r ../introduction_to_gwas/software .
+cp ../introduction_to_gwas/4.gwas/gwas_rrblup.R software
+cp ../introduction_to_gwas/4.gwas/gwas_sommer.R software
+cp ../introduction_to_gwas/1.preparatory_steps/prep_dogpheno.R software
 
 ## dry run
-
+snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs.svg
 
 ## full run
+#snakemake -s Snakefile_GWAS.binary
+
 
