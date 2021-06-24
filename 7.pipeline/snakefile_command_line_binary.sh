3511b67a964b4899730766a6cc55b0e02c9d7abb
filippob <filippo.biscarini@gmail.com>
2021-06-24 18:28:10 +0200

    fixed paths to software

--- 7.pipeline/snakefile_command_line_binary.sh
+++ 7.pipeline/snakefile_command_line_binary.sh
@@ -3,10 +3,10 @@
 
 mkdir data
 mkdir steps
-cp -r ../introduction_to_gwas/software .
-cp ../introduction_to_gwas/4.gwas/gwas_rrblup.R software
-cp ../introduction_to_gwas/4.gwas/gwas_sommer.R software
-cp ../introduction_to_gwas/1.preparatory_steps/prep_dogpheno.R software
+cp -r ../software .
+cp ../4.gwas/gwas_rrblup.R software
+cp ../4.gwas/gwas_sommer.R software
+cp ../1.preparatory_steps/prep_dogpheno.R software
 
 ## dry run
 snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs.svg
