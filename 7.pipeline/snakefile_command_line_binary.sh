9e92d9081a4511548786cfd9a490b222f8807210
filippob <filippo.biscarini@gmail.com>
2021-06-25 13:35:22 +0200

    changing name of svg DAG file

--- 7.pipeline/snakefile_command_line_binary.sh
+++ 7.pipeline/snakefile_command_line_binary.sh
@@ -9,7 +9,7 @@ cp ../4.gwas/gwas_sommer.R software
 cp ../1.preparatory_steps/prep_dogpheno.R software
 
 ## dry run
-snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs.svg
+snakemake --dag -n -s Snakefile_GWAS.binary | dot -Tsvg > dag_dogs_sommer.svg
 
 ## full run
 #snakemake -s Snakefile_GWAS.binary
