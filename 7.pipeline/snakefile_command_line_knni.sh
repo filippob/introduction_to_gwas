5fce9e7a9b85c162e211da032770b721685b604c
filippob <filippo.biscarini@gmail.com>
2022-05-17 14:40:23 +0200

    adding number of cores to the snakemake command line

--- 7.pipeline/snakefile_command_line_knni.sh
+++ 7.pipeline/snakefile_command_line_knni.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ## prepare the env
 
-mkdir data 2>/dev/null
-mkdir steps 2>/dev/null
+mkdir -p data 2>/dev/null
+mkdir -p steps 2>/dev/null
+
 cp -r ../software .
 cp ../4.gwas/gwas_* software ## which script do we want to use for GWAS?
 cp ../1.preparatory_steps/prep_rice_data_pipeline.R software
@@ -12,4 +13,4 @@ cp ../3.imputation/knni_tidymodels.R software
 ## dry run
 snakemake --dag -n -s Snakefile_GWAS.continuous_knni | dot -Tsvg > dag_knni.svg
 ## full run
-snakemake -s Snakefile_GWAS.continuous_knni
+snakemake -s Snakefile_GWAS.continuous_knni --cores 4
