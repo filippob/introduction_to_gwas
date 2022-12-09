1de692257438cee148bd2330364e8c558f28e1d3
filippob <filippo.biscarini@gmail.com>
2022-12-09 11:49:15 +0100

    cleaning up the code for the exercise

--- 8.collaborative_exercise/2.steps_filtering.sh
+++ 8.collaborative_exercise/2.steps_filtering.sh
@@ -3,12 +3,8 @@
 ## filtering genotype data ##
 #-----------------------------
 
-plink=plink
-MAF=0.05
-MISS_SAMPLE=0.1
-MISS_LOCUS=0.05
-THINNING=0.75
+## PARAMETERS
 
-$plink --file data/pigs --geno $MISS_LOCUS --mind $MISS_SAMPLE --maf $MAF --thin $THINNING --recode --out pigs_filtered
+## YOUR CODE HERE
 
-
+## COMMAND LINES
--- 8.collaborative_exercise/3.step_imputation.sh
+++ 8.collaborative_exercise/3.step_imputation.sh
@@ -3,23 +3,9 @@
 #--------------------------------------
 
 ## set paths to executables
-plink=plink
-beagle=beagle
-#beagle=/home/filippo/Documents/software/beagle5/beagle.28Sep18.793.jar
+## PARAMETERS
 
-## convert to vcf
-$plink --file pigs_filtered --recode vcf-iid --out pigs_filtered
+## YOUR CODE HERE
 
-## Beagle
-if [ $beagle = beagle ]; then
-	beagle gt=pigs_filtered.vcf out=pigs_imputed
-else
-	java -Xss5m -Xmx4g -jar $beagle gt=pigs_filtered.vcf out=pigs_imputed
-fi
+## COMMAND LINES
 
-## convert back to ped/map
-$plink --vcf pigs_imputed.vcf.gz --recode --double-id --out pigs_imputed
-
-rm pigs_imputed.vcf.gz
-
-echo "DONE!"
