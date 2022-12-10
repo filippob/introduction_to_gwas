8e9d384a12dd04ebcd15773f899d4555310c7d92
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-12-10 11:57:10 +0100

    Update install_from_bioconductor.r

--- 1.preparatory_steps/install_from_bioconductor.r
+++ 1.preparatory_steps/install_from_bioconductor.r
@@ -3,8 +3,9 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager", repos='http://cran.us.r-project.org') ## OK (Oscar):
 # BiocManager::install()
 
-if (!require("biomaRt")) BiocManager::install("biomaRt") ## OK (Oscar)
-if (!require("snpStats")) BiocManager::install("snpStats")
-BiocManager::install("qvalue")
+## UNCOMMENT TO INSTALL
+# if (!require("biomaRt")) BiocManager::install("biomaRt") ## OK (Oscar)
+# if (!require("snpStats")) BiocManager::install("snpStats")
+# BiocManager::install("qvalue")
 # BiocManager::install("gwasurvivr")
 
