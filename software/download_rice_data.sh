a64a30292a005e5f6103400a142372715aca1535
filippob <filippo.biscarini@gmail.com>
2024-06-26 22:01:04 +0200

    added: bash scripts to donwload and prepare rice data

---
+++ software/download_rice_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###################################
+## RICE DATA (continuous)
+###################################
+
+## set resources
+## Download data (rice, continuous)
+echo "Downloading rice data ..."
+
+### Check for dir, if not found create it using the mkdir ##
+[ ! -d "data" ] && mkdir -p "data"  # mkdir = make directory called data in relative path ".."
+
+cd data
+
+wget https://zenodo.org/record/50803/files/GBSgenotypes.tar.gz   # wget - retrieve content from web servers
+wget https://zenodo.org/record/50803/files/plantgrainPhenotypes.txt
+
+tar -xvzf GBSgenotypes.tar.gz  # extract files from archive
+
+wc -l GBSnew.ped # wc = word count, -l = count lines: Number of genotypes in ped file
+wc -l GBSnew.map # Number of markers in map file
+
+wc -l plantgrainPhenotypes.txt # Number of phenotypic records
+#less -S plantgrainPhenotypes.txt
+
+echo "DONE!"
--- software/get_rice_data.sh
+++ software/get_rice_data.sh
@@ -8,24 +8,7 @@ plink=plink  # directory with plink binary
 #plink=plink  # directory with plink binary
 trait=PH
 
-## Download data (rice, continuous)
-echo "Downloading rice data ..."
-
-### Check for dir, if not found create it using the mkdir ##
-[ ! -d "data" ] && mkdir -p "data"  # mkdir = make directory called data in relative path ".."
-
 cd data
-
-wget https://zenodo.org/record/50803/files/GBSgenotypes.tar.gz   # wget - retrieve content from web servers
-wget https://zenodo.org/record/50803/files/plantgrainPhenotypes.txt
-
-tar -xvzf GBSgenotypes.tar.gz  # extract files from archive
-
-wc -l GBSnew.ped # wc = word count, -l = count lines: Number of genotypes in ped file
-wc -l GBSnew.map # Number of markers in map file
-
-wc -l plantgrainPhenotypes.txt # Number of phenotypic records
-#less -S plantgrainPhenotypes.txt
 
 ## retrieve group information
 ## create new phenotypes file and ids file for Plink subsetting
