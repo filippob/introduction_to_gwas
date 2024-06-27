b82a6cd0f13e6ce9838b277e198091e1ccbc9e70
filippob <filippo.biscarini@gmail.com>
2024-06-27 14:25:36 +0200

    added: command lines for env prepping for continuous traits

---
+++ 7.pipeline/snakefile_command_line_continuous.sh
@@ -0,0 +1,8 @@
+mkdir data
+mkdir steps
+cp -r ../software .
+cp ../4.gwas/gwas_rrblup.R software
+cp ../4.gwas/gwas_statgengwas.R software
+cp ../1.preparatory_steps/prep_rice_data_pipeline.R software
+cp ../cross_reference/rice_group.reference software
+
